322e85ff343643a3297b0b294d3ed010c4872726
Jeff Wagner <jwagner@thoughtworks.com>
2016-07-13 16:51:30 -0500

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 var _ = require('underscore');
+var db = require('./db.js')
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -48,14 +49,13 @@ app.post('/todos', function (req, res) {
   var body = _.pick(req.body, 'description', 'completed');
   body.description = body.description.trim();
 
-  if (!_.isBoolean(body.completed) || !_.isString(body.description) || !body.description){
-    return res.status(400).send();
-  }
-
-  body.id = todoNextId++;
-  todos.push(body);
-
-  res.json(body);
+  db.todo.create(body)
+  .then(function (todo) {
+    res.json(todo.toJSON());
+  })
+  .catch(function (e) {
+    res.status(400).json(e);
+  });
 });
 
 app.delete('/todos/:id', function (req, res) {
@@ -101,7 +101,8 @@ app.put('/todos/:id', function (req, res) {
 
 });
 
-
-app.listen(PORT, function(){
-  console.log('Express listening on port: ' + PORT);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function(){
+    console.log('Express listening on port: ' + PORT);
+  });
 });
